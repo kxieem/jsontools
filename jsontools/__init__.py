05fa136e93fe2f32b489ba8d00084a4268bfccfa
Kaieem Patterson <102574528+kxieem@users.noreply.github.com>
2025-02-23 18:14:44 -0500

    Create __init__.py

---
+++ jsontools/__init__.py
@@ -0,0 +1,13 @@
+from .jsontools import (
+    jsonloader,
+    csvmaker,
+    flatten_dict,
+    extract_and_flatten
+)
+
+__all__ = [
+    "jsonloader",
+    "csvmaker",
+    "flatten_dict",
+    "extract_and_flatten"
+]
